b5b3260fd35c64c2bc188764e30616feb0e1323a
Dennis Alund <dennis.alund@gmail.com>
2022-12-14 21:42:38 +0100

    Renaming functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,8 @@
 // import * as billingFunctions from "./alerts/billing";
 // import * as appDistributionFunctions from "./alerts/app-distribution";
 
-import * as crashlyticsFunctions from "./alerts/crashlytics";
+export * from "./alerts/crashlytics";
 
-export const crashlytics = crashlyticsFunctions;
 // TODO: Uncomment when functions are supported. Keeping them commented out now
 // since the plugin is being published and the functions aren't doing anything.
 // export const performance = performanceFunctions;
