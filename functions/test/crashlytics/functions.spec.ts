405db21591e8a52987c764ae626dca76beca8162
Dennis Alund <dennis.alund@gmail.com>
2023-08-29 15:07:55 +0800

    Fixing lint

--- functions/test/crashlytics/functions.spec.ts
+++ functions/test/crashlytics/functions.spec.ts
@@ -1,13 +1,12 @@
-import { describe, it } from "mocha";
+import {describe, it} from "mocha";
 import {expect} from "chai";
 
 describe("functions/test/crashlytics/functions.spec.ts", () => {
-
   // ===========================================================================
   // ===========================================================================
 
   describe("onNewAnrIssuePublished", () => {
-    it("should just return true", async () => {  
+    it("should just return true", async () => {
       expect(true).to.be.true;
     });
   });
