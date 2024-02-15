aad6feea6ff8831e1f815c9c4d943e3fb057ac8e
Dennis Alund <dennis.alund@gmail.com>
2024-02-15 13:58:29 +0800

    Removing dysfunctional test

--- functions/test/unit/crashlytics/functions.spec.ts
+++ functions/test/unit/crashlytics/functions.spec.ts
@@ -2,9 +2,6 @@ import {describe, it} from "mocha";
 import {expect} from "chai";
 
 describe("functions/test/crashlytics/functions.spec.ts", () => {
-  // ===========================================================================
-  // ===========================================================================
-
   describe("onNewAnrIssuePublished", () => {
     it("should just return true", async () => {
       expect(true).to.be.true;
